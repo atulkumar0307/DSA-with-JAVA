a28a96e50bcbfe14417296e80527289f9c008f34
Atul Kumar <atulkumar030303@gmail.com>
2026-07-23 22:24:20 +0530

    Buy and Sell Stock

---
+++ ArrayTwo/BuyAndSellStocks.java
@@ -0,0 +1,24 @@
+package ArrayTwo;
+
+public class BuyAndSellStocks {
+
+    public static int maxProfit(int prices[]){
+        int minBuyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        minBuyPrice = prices[0];
+        for(int i=1; i<prices.length; i++){
+            if(minBuyPrice < prices[i]){ // profit
+                int Profit = prices[i] - minBuyPrice; // today profit
+                maxProfit = Math.max(maxProfit, Profit);  // max profit
+            }else{
+                minBuyPrice = prices[i]; // update buy price
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.print("Max Profit: " + maxProfit(prices));
+    }
+}
