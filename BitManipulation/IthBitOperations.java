13a833c760e32c2654145f68e18d8e0f290e5ec2
Atul Kumar <atulkumar030303@gmail.com>
2026-08-03 23:57:24 +0530

    Ith bit operations 2

--- BitManipulation/IthBitOperations.java
+++ BitManipulation/IthBitOperations.java
@@ -39,11 +39,20 @@ public class IthBitOperations {
         return n & bitmask;
     }
 
+    // Its logic is very important, do pen paper dry run and understand flow;
+    public static int clearBitsInRange(int n, int i, int j){
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitmask = a | b;
+        return n & bitmask;
+    }
+
     public static void main(String[] args) {
         // System.out.println(getIthBit(10, 3));
         // System.out.println(setIthBit(10, 2));
         // System.out.println(clearIthBit(10, 1));
         // System.out.println(updateIthBit(10, 2, 1));
-        System.out.println(clearLastithBits(15, 2));
+        // System.out.println(clearLastithBits(15, 2));
+        // System.out.println(clearBitsInRange(10, 2, 4));
     }
 }
